7e603ebb088ac2f27a8e51f081609bd0f6df5750
Fernando Muller <fernando.muller096@gmail.com>
2022-07-04 21:32:02 -0300

    correct lambda artifacts path in .sh script

--- lambda_artifacts.sh
+++ lambda_artifacts.sh
@@ -1,7 +1,7 @@
 echo "========== Starting Lambda artifacts creation =========="
 
 echo "========== Entering lambda worker directory"
-cd infra/terraform/workingdir/modules/sqs_lambda_s3/lambda_scripts/worker
+cd infra/terraform/workingdir/modules/aws-data-pipeline/lambda_scripts/worker
 
 echo "========== Ziping .py files =========="
 for file in *; do zip -r ${file%.*}.zip $file; done
